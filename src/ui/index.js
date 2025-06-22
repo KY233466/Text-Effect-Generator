bd38cab721c19f0059b9497c5c484ac8fb26c308
KY233466 <zihan.yang@tufts.edu>
2025-06-21 22:23:24 -0400

    attempt to add app.jsx

--- src/ui/index.js
+++ src/ui/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import addOnUISdk from "https://new.express.adobe.com/static/add-on-sdk/sdk.js";
 import opentype from "opentype.js";
 import TextWarpApp from "./TextWarpApp.js";
+import App from "./App.jsx";
 
 // 将 opentype 添加到全局对象，供其他组件使用
 window.opentype = opentype;
@@ -12,5 +13,5 @@ addOnUISdk.ready.then(async () => {
   const sandboxProxy = await runtime.apiProxy("documentSandbox");
   const container = document.getElementById("root");
   const root = createRoot(container);
-  root.render(React.createElement(TextWarpApp, { sandboxProxy }));
+  root.render(React.createElement(App, { sandboxProxy }));
 }); 
